c8221f5daa5c17b4f8fff9752b96747184d63f75
sagarhasan273 <sagarhasan273@gmail.com>
2024-02-04 01:13:22 +0600

    User Interface completed!

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import myImage from "../images/gmail.png";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function Home() {
   return (
-    <div>
-      Home
-    </div>
-  )
+    <>
+      <div className="container mt-3">
+        <div className="d-flex justify-content">
+          <h2>Send Email with Kobo Mail</h2>
+          <img
+            src={myImage}
+            alt="not found"
+            className="mx-3"
+            style={{ width: "50px" }}
+          />
+        </div>
+        <div className="d-flex justify-content">
+          <Form className="mt-2 col-lg-6">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Enter your Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
